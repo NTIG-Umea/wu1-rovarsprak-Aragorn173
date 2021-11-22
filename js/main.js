9983023d7faca2ded9155130e989c521bb77e7ee
Aragorn173 <77720219+Aragorn173@users.noreply.github.com>
2021-11-22 13:39:28 +0100

    did the stuff

--- js/main.js
+++ js/main.js
@@ -15,17 +15,44 @@ document.addEventListener('DOMContentLoaded', () => {
         // skicka texten till funktionen för att översätta
         const translation = translate(text);
         // Hur kan du rensa textboxen?
+        textbox.value = "";
         // skriv ut översättningen i message
         display(translation, message);
     });
-
+    textbox.addEventListener('change', (e) => {
+        // läs in texten från textboxen
+        const text = textbox.value;
+        // skicka texten till funktionen för att översätta
+        const translation = translate(text);
+        // Hur kan du rensa textboxen?
+        textbox.value = "";
+        // skriv ut översättningen i message
+        display(translation, message);
+    });
 });
 
-const translate = (text) => {
+//const translate = (text) => {
     // översätt texten till rövarspråket
-    console.log(text)
-    return text;
-};
+   // let translation = "";
+   // let kons = "bcdfghjklmnpqrstvxz"
+
+    //for (i = 0; i < text.length; i++)
+     //   console.log(text[i]);
+
+    
+    const translate = (text) => {
+        var string = text.toLowerCase();
+        var vowels = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z", " "];
+        var translation = "";
+        for (i = 0; i < string.length; i++) {
+            var current = string.charAt(i); 
+            if (vowels.indexOf(current) == -1)
+            translation= (translation + (current));
+            else 
+              translation = (translation + (current + "o" + current));
+            }
+        return translation;
+      };
 
 const display = (translation, element) => {
     // skapa ett p element för texten
